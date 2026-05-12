46c2e7a83d20aa673f3e0ccafbe981308c8b97e3
harsha <harshanavuluri@gmail.com>
2026-05-12 09:07:41 +0530

    Fixed production CORS

--- backend/src/main/java/com/devvault/backend/config/CorsConfig.java
+++ backend/src/main/java/com/devvault/backend/config/CorsConfig.java
@@ -1,28 +1,21 @@
 package com.devvault.backend.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
-public class CorsConfig {
+public class CorsConfig implements WebMvcConfigurer {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
 
-        return new WebMvcConfigurer() {
-
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-
-                registry.addMapping("/**")
-                        .allowedOrigins("*")
-                        .allowedMethods("*")
-                        .allowedHeaders("*");
-            }
-        };
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost:3000",
+                        "https://dev-vault-theta.vercel.app"
+                )
+                .allowedMethods("*")
+                .allowedHeaders("*");
     }
 }
